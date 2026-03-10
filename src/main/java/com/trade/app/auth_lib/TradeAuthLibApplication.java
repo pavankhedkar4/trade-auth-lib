91ac9da1bf751d57eb8208fb79c48f5d5c91cc1c
pavankhedkar4 <pavankhedkar4@gmail.com>
2026-03-10 10:37:45 +0530

    Initial commit

---
+++ src/main/java/com/trade/app/auth_lib/TradeAuthLibApplication.java
@@ -0,0 +1,13 @@
+package com.trade.app.auth_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeAuthLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeAuthLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trade/app/auth_lib/TradeAuthLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.app.auth_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeAuthLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
